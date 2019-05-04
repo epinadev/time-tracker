907ce2d41dfdf7ce58195fd5b2bcb253457a0df4
Eduardo Piña <epinaster@gmail.com>
2019-05-04 00:52:20 -0500

    Initial stage of the timer

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import StopWatch from './StopWatch';
 
-const styles = theme => ({
+const styles = theme => {
+    console.log(theme) 
+    return {
     content : {
         backgroundColor: '#FFF'
-    }
-})
+    },
+}}
 
 function Task({ classes }) {
 
     return(
-        <Grid item xs={3} className={classes.content}>
-            taskArea
+        <Grid item xs={4} className={classes.content}>
+            <StopWatch/>
         </Grid>
     ) 
 }
