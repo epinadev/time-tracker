6a999b14a546fc903602a272b851c06f7a082eb8
Eduardo Piña <epinaster@gmail.com>
2019-05-06 22:51:00 -0500

    Download file as txt

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -6,8 +6,10 @@ import DashboardIcon from '@material-ui/icons/Dashboard';
 import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
+import CloudDownload from '@material-ui/icons/CloudDownload';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import {  withStyles } from "@material-ui/core/styles";
+import { TaskContext } from '../store/TaskContext';
 
 const styles = theme => ({
     icon: {
@@ -17,6 +19,16 @@ const styles = theme => ({
 
 function SideBar (props) {
     const { classes } = props;
+    const { tasks } = useContext(TaskContext);
+    const handleDownload = () => {
+        const taskData = tasks.map(t => `${t.description}, ${t.hours}:${t.mins}:${t.secs}\n`);
+        const data = new Blob(taskData, {type: 'text/csv'});
+        const csvURL = window.URL.createObjectURL(data);
+        let tempLink = document.createElement('a');
+        tempLink.href = csvURL;
+        tempLink.setAttribute('download', 'tasks.txt');
+        tempLink.click();
+    }
     return(
         <div>
             <ListItem button>
@@ -26,11 +38,11 @@ function SideBar (props) {
                 <ListItemText primary="Dashboard" />
             </ListItem>
 
-            <ListItem button>
+            <ListItem button onClick={handleDownload}>
                 <ListItemIcon className={classes.icon}>
-                    <AssignmentIcon/>
+                    <CloudDownload/>
                 </ListItemIcon>
-                <ListItemText primary="Orders" />
+                <ListItemText primary="Dashboard" />
             </ListItem>
 
             <ListItem button>
